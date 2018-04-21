2fdcf4334286d9ccc74868f0b0e870d7a903ec9f
jjvs92 <julian_jvs92@hotmail.com>
2018-04-20 23:18:28 -0500

    homework that will be turned in

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,14 @@
 // Declaring varialbes 
-var computerChoices = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" ,"l" , "m", "n", "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
+var computerChoices = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" ,"l" , "m", "n", "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var guessedLetters = []
+var guessedLetters = [];
 var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// created array to not allow other keys manipulate the game
+var alphaKeys = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" ,"l" , "m", "n", "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"];
+
+
 
 //Creating the reset function to begin the game
 function reset() {
@@ -19,43 +23,43 @@ reset();
 
 console.log(compGuess);
 
-// Creating the User guess with onKeyUp function 
+// Creating function that runs the game
 document.onkeyup = function(event){
-
+// creating user guess
     var userGuess = event.key;
 
+    
     console.log(userGuess);
 
     console.log(compGuess);
 
-    // Creating if statement to only accepet alpha keys
-
+    // Creating if statement to only accept alpha keys (tried multiple things and did not resolve it)
     
-        // If the user guesses the correct letter, they win, and game restarts 
-        if (userGuess === compGuess){
-        alert("You got it!");
-        guessesLeft = 10;
-        wins++;
-        document.getElementById("guessesLeft").innerHTML= "Guesses Left: " + guessesLeft;
-        guessedLetters= [];
-        document.getElementById("wins").innerHTML= "Wins:" + wins;
-        compGuess= computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        } 
+    // If the user guesses the correct letter, they win, and game restarts 
+
+            if (userGuess === compGuess){
+                alert("You got it!");
+                guessesLeft = 10;
+                wins++;
+                document.getElementById("guessesLeft").innerHTML= "Guesses Left: " + guessesLeft;
+                guessedLetters= [];
+                document.getElementById("wins").innerHTML= "Wins:" + wins;
+                compGuess= computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                } 
+                
+                // If user does not guess the correct letter
+                else{
+                    // check if the letter has been used
+                    if(guessedLetters.indexOf(userGuess) >= 0){
+                        alert("This letter was already guessed");
+                    } 
+                    else {
+                    //take userGuess and add to array of guessedLetters and udpate guessesLeft to one less
+                    guessesLeft--;
+                    guessedLetters.push(userGuess);
+                    document.getElementById("guessesLeft").innerHTML= "Guesses Left: " + guessesLeft;
+                    }}       
         
-        // If user does not guess the correct letter
-        else{
-
-            if(guessedLetters.indexOf(userGuess) >= 0){
-                alert("This letter was already guessed");
-            } 
-            else {
-            //take userGuess and add to array of guessedLetters and udpate guessesLeft to one less
-            guessesLeft--;
-            guessedLetters.push(userGuess);
-            document.getElementById("guessesLeft").innerHTML= "Guesses Left: " + guessesLeft;
-            document.getElementById("guessedLetters").innnerHTML= "Guessed letters: " + guessedLetters + " , ";
-            }}
-
     // Setting condition if player runs out of guesses
     if (guessesLeft == 0){
         guessesLeft = 10;
@@ -66,18 +70,11 @@ document.onkeyup = function(event){
         document.getElementById("guessesLeft").innerHTML= "Guesses Left: " + guessesLeft;
         alert("Game Over!");
     } 
+    // This will update the html to show the guessed letters
+    document.getElementById("guessedLetters").innerHTML= "Guessed letters: " + guessedLetters;
 
-    lettersGuessed();
 
 }
-
-
-
-
-function lettersGuessed(){
-    document.getElementById("guessedLetters").innerHTML= "Guessed letters: " + guessedLetters
-}
-
 
 
 
